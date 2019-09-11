76980910ff08a8d0b6b42fe5adc95b93f6a8b943
yashinomi12 <kaoribako12@gmail.com>
2019-09-11 10:14:10 +0900

    fixed contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,3 @@
 class Contact < ApplicationRecord
-  validates :email, presense: true, length: {maximum:255},
-                    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-  validates :message, presence: true
+
 end
