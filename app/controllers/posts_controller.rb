7a548e07e9a290076ca9e56fe0d183e8f8cc0794
yashinomi12 <kaoribako12@gmail.com>
2019-10-08 21:29:53 +0900

    postcontroller before_action fix2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user, {only: [:new, :create, :edit, :update, :destroy]}
+  before_action :authenticate_user, {only: [:new, :show, :create, :edit, :update, :destroy]}
   def index
     @posts = Post.all
   end
