bae4142b9b178c8ab9367bc30d332eeac58f340a
yashinomi12 <kaoribako12@gmail.com>
2019-10-15 09:15:04 +0900

    production.rb fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,16 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "mayuen_#{Rails.env}"
-
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    domein: 'smtp.gmail.com',
+    user_name: 'kaoribako12@gmail.com',
+    password: 'yashi1223',
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
+  
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
